4a5069c65762be3ff7d83addfe0e7cb5cb1b7d85
Ray Zaranza <rayzaranza@gmail.com>
2026-03-30 14:49:53 -0300

    feat: implement login and logout with github oauth

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,24 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 import svgr from "vite-plugin-svgr";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    tanstackRouter({ target: "react", autoCodeSplitting: true }),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      generatedRouteTree: "./src/lib/routeTree.gen.ts",
+    }),
     react(),
     svgr(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
