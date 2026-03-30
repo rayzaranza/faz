ec140dc2f1409dc14231e473a044bfae9afdd22e
rayzaranza <rayzaranza@gmail.com>
2026-03-30 14:27:33 -0300

    feat: add loading state to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,11 @@
 import { cn } from "../utils/classNames";
-import { type LucideIcon } from "lucide-react";
+import { Loader, type LucideIcon } from "lucide-react";
 import { Icon } from "./Icon";
 
 interface ButtonProps extends React.ComponentPropsWithRef<"button"> {
   variant?: "default" | "accent" | "danger";
   icon?: LucideIcon;
+  isLoading?: boolean;
 }
 
 const variants = {
@@ -19,23 +20,29 @@ export function Button({
   children,
   variant = "default",
   icon,
+  isLoading = false,
   ...rest
 }: ButtonProps) {
   return (
     <button
       type="button"
       className={cn(
-        "flex h-10 cursor-pointer items-center justify-center gap-2 rounded-2xl px-4 text-lg select-none disabled:cursor-not-allowed",
+        "flex h-10 cursor-pointer items-center justify-center gap-2 rounded-2xl px-4 pb-0.5 text-lg select-none disabled:cursor-not-allowed",
         "shadow-elevated hover:shadow-elevated-hover active:shadow-inset",
         "transition hover:-translate-y-px active:translate-y-0.5",
         "squircle",
         !children && "w-10",
         variants[variant],
+        isLoading &&
+          "pointer-events-none cursor-progress bg-surface-pressed shadow-inset",
       )}
       {...rest}
     >
-      {icon && <Icon size="medium" icon={icon} />}
-      {children && <span className="translate-y-[0.1rem]">{children}</span>}
+      {isLoading && (
+        <Icon size="medium" icon={Loader} className="animate-spin" />
+      )}
+      {!isLoading && icon && <Icon size="medium" icon={icon} />}
+      {children && <span className="translate-y-[0.05rem]">{children}</span>}
     </button>
   );
 }
